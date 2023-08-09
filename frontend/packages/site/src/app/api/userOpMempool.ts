1b1828b07d87cb5e650e172697ed81e20b4ac5f5
mm <macalonabc30@gmaile.com>
2023-08-09 10:35:32 +0900

    add:POAT userOpMempooの入力の方定義を追加

--- frontend/packages/site/src/app/api/userOpMempool.ts
+++ frontend/packages/site/src/app/api/userOpMempool.ts
@@ -6,6 +6,38 @@ dotenv.config();
 export async function POST(req: NextApiRequest,res: NextApiResponse) {
     const sdk = require('api')('@stackup/v0.6#9j72valhd1dd90');
 
+
+    /**
+     * 
+     * sender: オペレーションの送信者
+     * nonce: ユーザーの nonce
+     * initCode: 初期化コード
+     * callData: コールデータ
+     * callGasLimit: コールのガスリミット
+     * verificationGasLimit: 検証のガスリミット
+     * preVerificationGas: 事前検証のガス
+     * maxFeePerGas: ガスあたりの最大手数料
+     * maxPriorityFeePerGas: ガスあたりの最大優先手数料
+     * paymasterAndData: Paymaster とデータ
+     * signature: 署名
+     * 
+     */
+    interface op {
+        sender: string;
+        nonce: string;
+        initCode: string;
+        callData: string;
+        callGasLimit: string;
+        verificationGasLimit: string;
+        preVerificationGas: string;
+        maxFeePerGas: string;
+        maxPriorityFeePerGas: string;
+        paymasterAndData: string;
+        signature: string;
+    }
+
+    const op:op = req.body.op
+
     try {
         const uoHash = await sdk.ethSenduseroperation(
             {
@@ -13,7 +45,7 @@ export async function POST(req: NextApiRequest,res: NextApiResponse) {
                 id: req.body.id,
                 method: 'eth_sendUserOperation',
                 params: [
-                    req.body.op,
+                    op,
                     '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'
                 ]
             }, {
